55d8286a3525fba2ced76c81f22af1f55d90826f
aygaoua <zgaoua72@gmail.com>
2024-01-21 12:16:13 +0000

    try with an other metode

--- ex04/main.cpp
+++ ex04/main.cpp
@@ -10,39 +10,100 @@
 /*                                                                            */
 /* ************************************************************************** */
 
+// #include <iostream>
+// #include <string>
+// #include <fstream>
+// #include <sstream>
+
+// int main(int ac, char **av) {
+//     std::ifstream file(av[3]);
+//     if (file.is_open())
+//     {
+//         std::stringstream buffer;
+//         buffer << file.rdbuf();
+//         std::string fileContent = buffer.str();
+
+
+//         std::string S1 = av[1];
+//         std::string S2 = av[2];
+
+//         size_t i = fileContent.find(S1);
+//         while (i) {
+//             if (i != 0) {
+//                 fileContent.erase(i, S1.length());
+//                 fileContent.insert(i, S2);
+//             }
+//             i = fileContent.find(S1);
+//         }
+//         std::cout << fileContent << std::endl;
+//     }
+//     else {
+//         std::cout << "the file is not openable !!" << std::endl;
+//     }
+//     return (0);
+// }
+
+// //find
+// //erase
+// //insert
+
 #include <iostream>
-#include <string>
 #include <fstream>
-#include <sstream>
+#include <string>
 
-int main(int ac, char **av) {
-    std::ifstream file(av[3]);
-    if (file.is_open())
+
+int main(int argc, char **argv)
+{
+    std::fstream infile;
+    std::ofstream outfile;
+    std::string line;
+    std::string outfilename;
+	std::string searchedstr;
+	std::string newstr;
+	size_t pos;
+
+    if (argc != 4)
     {
-        std::stringstream buffer;
-        buffer << file.rdbuf();
-        std::string fileContent = buffer.str();
-
-
-        std::string S1 = av[1];
-        std::string S2 = av[2];
-
-        size_t i = fileContent.find(S1);
-        while (i) {
-            if (i != 0) {
-                fileContent.erase(i, S1.length());
-                fileContent.insert(i, S2);
+        std::cout << "usage: ./sed [filename] [string 1] [string 2]" << std::endl;
+        return 1;
+    }
+	newstr = std::string(argv[3]);
+	searchedstr = std::string(argv[2]);
+    infile.open(argv[1]);
+    if (infile.is_open())
+    {
+        //file is open
+        outfilename = std::string(argv[1]) + ".replace";
+        outfile.open(outfilename);
+        while (std::getline(infile, line))
+        {
+            if (line.compare(searchedstr) == 0)
+            {
+                //found a string
+                outfile << newstr << std::endl;
             }
-            i = fileContent.find(S1);
+			else if (line.find(searchedstr, 0) != std::string::npos)
+			{
+				while (line.find(searchedstr, 0) != std::string::npos)
+				{
+					pos = line.find(searchedstr, 0);
+					line.erase(line.find(searchedstr), searchedstr.length());
+					line.insert(pos, newstr);
+				}
+				outfile << line << std::endl;
+			}
+            else
+            {
+                outfile << line << std::endl;
+            }
         }
-        std::cout << fileContent << std::endl;
+        infile.close();
+        outfile.close();
     }
-    else {
-        std::cout << "the file is not openable !!" << std::endl;
+    else
+    {
+        std::cout << "Unable to open file " << argv[1];
+        return 1;
     }
-    return (0);
+    return 0;
 }
-
-//find
-//erase
-//insert
