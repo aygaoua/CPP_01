6ce55694d9eae7a5f563a5ffd1f6ab9d3dc6d3f6
aygaoua <102914808+aygaoua@users.noreply.github.com>
2024-01-21 21:09:55 +0100

    making the main more clear

--- ex06/main.cpp
+++ ex06/main.cpp
@@ -6,18 +6,18 @@
 /*   By: azgaoua <azgaoua@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/01/21 18:12:58 by azgaoua           #+#    #+#             */
-/*   Updated: 2024/01/21 19:59:06 by azgaoua          ###   ########.fr       */
+/*   Updated: 2024/01/21 21:07:18 by azgaoua          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "Harl.hpp"
 
 int main(int ac , char **av) {
-    Harl harl;
     if (ac != 2) {
-        std::cout << "Usage: ./harl (DEBUG || INFO || WARNING || ERROR)" << std::endl;
+        std::cout << "Usage: ./harlFilter (DEBUG || INFO || WARNING || ERROR)" << std::endl;
         return (0);
     }
-    harl.complain(av[1]);
+    Harl harlFilter;
+    harlFilter.complain(av[1]);
     return (0);
 }
